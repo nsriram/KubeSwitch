b5d59cfc1ce332bba46eef5e34e66cd079d0a94c
Sriram <sriramn@thoughtworks.com>
2019-05-15 22:53:15 +0800

    [Sriram] Simple build script to build from command line

--- build.sh
+++ build.sh
@@ -1,3 +1,47 @@
-xcodebuild clean build test -workspace KubeSwitch.xcworkspace \
+#!/bin/bash
+
+clean() {
+xcodebuild clean -workspace KubeSwitch.xcworkspace \
   -scheme KubeSwitch \
   -destination 'platform=OS X,arch=x86_64' | xcpretty
+}
+
+build() {
+xcodebuild build -workspace KubeSwitch.xcworkspace \
+  -scheme KubeSwitch \
+  -destination 'platform=OS X,arch=x86_64'  | xcpretty
+}
+
+test() {
+xcodebuild test -workspace KubeSwitch.xcworkspace \
+  -scheme KubeSwitch \
+  -destination 'platform=OS X,arch=x86_64' | xcpretty	
+}
+
+clean_and_build() {
+  clean
+  build
+}
+
+if [ $# -eq 0 ]; then
+	echo "[Info] Default clean and build"
+	clean_and_build
+    exit
+fi
+
+if [ "$1" == "build" ]; then
+	echo "[Info] Executing clean and build"
+	clean_and_build
+elif [ "$1" == "test" ]; then
+	echo "[Info] Executing clean, build, test"
+	clean_and_build
+	test
+elif [ "$1" == "clean" ]; then
+	echo "[Info] Executing clean"
+	clean
+else
+	echo "[Warn] Invalid option"
+	echo "[Warn] Usage: build.sh clean (or) build.sh build (or) build.sh test"
+fi
+
+exit
